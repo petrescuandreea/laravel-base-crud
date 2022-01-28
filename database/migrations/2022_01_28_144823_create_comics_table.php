b11247058fc1d080708f7402319418a408caf2bf
Andreea1696 <andreeapetrescu16@gmail.com>
2022-01-28 15:59:25 +0100

    Create comics migration

---
+++ database/migrations/2022_01_28_144823_create_comics_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+
+            $table -> id();
+
+            $table -> string('title');
+            $table -> string('author');
+            $table -> date('release_date');
+            $table -> count('pages');
+            
+            $table -> timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+}
