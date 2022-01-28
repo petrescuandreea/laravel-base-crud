fd84a0e2727fa12ac4b2aeede9d030e043edac91
Andreea1696 <andreeapetrescu16@gmail.com>
2022-01-28 17:17:02 +0100

    Update content in home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,19 @@
 @extends('layouts.main-layout')
 @section('content')
-    <h2>
-        Home
-    </h2>
+    <section>
+        <h2>Comics list:</h2>
+
+        <ul>
+            @foreach ($comics as $comic)
+
+                <li>
+                    <a href="#"> 
+                        {{ $comic -> title }} 
+                    </a> 
+                    - {{ $comic -> release_date }}
+                </li>
+                
+            @endforeach
+        </ul>
+    </section>
 @endsection
